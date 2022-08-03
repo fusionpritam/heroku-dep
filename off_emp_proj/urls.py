5894102dfe87798a7ce59980fab6f56b9a59929b
Pritam Bhattacharya <83444990+fusionpritam@users.noreply.github.com>
2022-08-03 14:53:31 +0530

    Update urls.py

--- off_emp_proj/urls.py
+++ off_emp_proj/urls.py
@@ -17,15 +17,16 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 
-
-from django.views.static import serve
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.views.static import serve
+# from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('emp_app.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+#     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+#     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # urlpatterns=urlpatterns+static(settings.MEDIA_URL.)
